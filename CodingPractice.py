ab93070b3ed55e59585d50470116a75fad495cea
TJScott3 <judahscott07@outlook.com>
2025-11-13 18:47:20 -0500

    weather check program

---
+++ CodingPractice.py
@@ -0,0 +1,9 @@
+# Coding Practice Weather thing
+
+answer=input("Is it sunny outside? Type yes or no:")
+
+if answer == "yes":
+  print("have fun!")
+else:
+  print("might want to bring an umbrella!")
+
