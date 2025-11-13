1d0f5d2c67b86bf05a08137a50143fddd5268eef
TJScott3 <judahscott07@outlook.com>
2025-11-13 18:07:55 -0500

    Add number guessing game functionality
    Implement a number guessing game that provides hints.

--- Script.py
+++ Script.py
@@ -1,1 +1,25 @@
+import random
 
+secret = random.randint(1, 10)
+
+print("I'm thinking of a number between 1 and 10.")
+print("Try to guess it!")
+
+# Start with a placeholder guess
+guess = 0
+
+# Loop until the guess is correct
+while guess != secret:
+    text = input("Your guess: ")
+    guess = int(text)
+
+    # Give hints
+    if guess < secret:
+        print("Too low.")
+    if guess > secret:
+        print("Too high.")
+    if guess == secret:
+        print("That's right!")
+        print("You win!")
+
+print("Thanks for playing!") 
